c68cbac4ed3f45f5ca1ae4b804cae09d82defc89
Azarcon18 <azarconrob@gmail.com>
2024-12-10 20:49:44 +0800

    commit

--- classes/registereduser_login.php
+++ classes/registereduser_login.php
@@ -46,6 +46,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['name'] = $user['name'];
                 $_SESSION['email'] = $user['email'];
 
+                // Set a session variable to indicate successful login
+                $_SESSION['login_success'] = true;
+
                 $stmt->close();
                 $conn->close();
 
