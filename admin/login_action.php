a50613d36889aeca77f982f7947a1a6b838450fd
Azarcon18 <azarconrob@gmail.com>
2024-12-11 07:51:54 +0800

    commit

--- admin/login_action.php
+++ admin/login_action.php
@@ -1,10 +1,4 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
 session_start();
 require_once('../config.php');
 
