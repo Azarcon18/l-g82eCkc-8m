3e03006e64e468cd138abcf574e7d141d4b890cc
Azarcon18 <azarconrob@gmail.com>
2024-12-10 13:42:06 +0800

    commit

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,11 @@
-
+<?php
+  // require_once('sess_auth.php');
+  header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Permissions-Policy: geolocation=(self), microphone=()")
+?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
