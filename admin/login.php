ae80eda72765291173e95a0f0c7dc7b2c07f3182
Azarcon18 <azarconrob@gmail.com>
2024-12-11 09:34:01 +0800

    new

--- admin/login.php
+++ admin/login.php
@@ -1,10 +1,4 @@
 <?php require_once('../config.php');
-$request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
 ?>
 <!DOCTYPE html>
 <html lang="en" style="height: auto;">
