9ccc43794588731f066406eacb13d25ee9f538be
Azarcon18 <azarconrob@gmail.com>
2024-12-11 08:03:07 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,4 @@
-<?php require_once('../config.php'); $request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}?>
+<?php require_once('../config.php'); ?>
  <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
 <?php require_once('inc/header.php') ?>
@@ -29,6 +24,12 @@ if (substr($request, -4) == '.php') {
                   include $page.'.php';
 
               }
+              $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
             ?>
           </div>
         </section>
